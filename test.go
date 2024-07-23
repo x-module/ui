b1374818c8b3a589255bbc8aaa7ed4c23dd076af
lijin <244395692@qq.com>
2024-07-23 09:22:12 +0800

    init

--- test.go
+++ test.go
@@ -3,6 +3,7 @@ package main
 //
 //import (
 //	"fmt"
+//	tips2 "game.test.client/ui/pages/tips"
 //	"game.test.client/widgets"
 //	"gioui.org/app"
 //	"gioui.org/layout"
@@ -11,23 +12,29 @@ package main
 //	"gioui.org/widget"
 //	"gioui.org/widget/material"
 //	"github.com/chapar-rest/chapar/ui/chapartheme"
+//	"github.com/chapar-rest/chapar/ui/explorer"
+//	widgets2 "github.com/chapar-rest/chapar/ui/widgets"
 //	"time"
 //)
 //
 //var modal *widgets.Modal
 //var clickable *widget.Clickable
-//
-//var theme = chapartheme.New(material.NewTheme(), true)
+//var th = material.NewTheme()
+//var theme = chapartheme.New(th, true)
 //var Prompt *widgets.Prompt = widgets.NewPrompt("", "", "")
 //var NotificationController = &widgets.Notification{}
 //var badgeInput *widgets.BadgeInput
+//var tips *tips2.Tips
+//
+//var binaryFile *widgets2.BinaryFile
+//var flatButton *widgets.FlatButton
 //
 //func main() {
 //	modal = widgets.NewModal(theme)
 //	modal.SetTitle("玩家登录")
 //	modal.SetHeight(200)
 //	clickable = new(widget.Clickable)
-//
+//	binaryFile = widgets2.NewBinaryFile("/Users/lijin/go/gioui/go.mod")
 //	items := []*widgets.BadgeInputItem{
 //		{
 //			Identifier: "bbb",
@@ -39,10 +46,26 @@ package main
 //		},
 //	}
 //	badgeInput = widgets.NewBadgeInput(items...)
+//	tips = tips2.New()
+//
+//	flatButton = &widgets.FlatButton{
+//		Icon:         widgets.ArrowDropDownIcon,
+//		IconPosition: 1,
+//		SpaceBetween: unit.Dp(10),
+//		Text:         "aaa",
+//		Clickable:    clickable,
+//
+//		BackgroundPadding: unit.Dp(1),
+//		CornerRadius:      5,
+//		MinWidth:          unit.Dp(120),
+//		BackgroundColor:   theme.SideBarBgColor,
+//		TextColor:         theme.Palette.Fg,
+//		ContentPadding:    unit.Dp(5),
+//		MarginRight:       10,
+//	}
 //
 //	go func() {
 //		w := new(app.Window)
-//		th := material.NewTheme()
 //		var ops op.Ops
 //		for {
 //			e := w.Event()
@@ -51,6 +74,8 @@ package main
 //				panic(e.Err)
 //			case app.FrameEvent:
 //				gtx := app.NewContext(&ops, e)
+//				//tips.Layout(gtx, theme)
+//				//binaryFile.Layout(gtx, theme)
 //				modalLayout(gtx, th)
 //				e.Frame(gtx.Ops)
 //			}
@@ -110,27 +135,37 @@ package main
 //	Prompt.Show()
 //}
 //
+//var checkBox = &widget.Bool{
+//	Value: true,
+//}
+//var explore = &explorer.Explorer{}
+//
 //func modalLayout(gtx layout.Context, th *material.Theme) layout.Dimensions {
 //	if clickable.Clicked(gtx) {
+//		fmt.Println("asdfasdfasdfasd")
+//		fmt.Println("asdfasdfasdfasd")
+//		fmt.Println("asdfasdfasdfasd")
+//		fmt.Println("asdfasdfasdfasd")
 //		//modal.Display(func(gtx layout.Context) layout.Dimensions {
 //		//	//return material.Label(th, unit.Sp(16), "modalState.Content").Layout(gtx)
 //		//	return data(gtx, th)
 //		//})
 //		//Send("success", 10*time.Second)
 //
-//		options := []widgets.Option{
-//			{
-//				Text: "aaa",
-//				Icon: widgets.ArrowDropDownIcon,
-//			},
-//			{
-//				Text: "bbbb",
-//				Icon: widgets.ExpandIcon,
-//			},
-//		}
+//		//options := []widgets.Option{
+//		//	{
+//		//		Text: "aaa",
+//		//		Icon: widgets.ArrowDropDownIcon,
+//		//	},
+//		//	{
+//		//		Text: "bbbb",
+//		//		Icon: widgets.ExpandIcon,
+//		//	},
+//		//}
+//		//
+//		//ShowPrompt("title", "content", widgets.ModalTypeErr, func(selectedOption string, remember bool) {
+//		//}, options...)
 //
-//		ShowPrompt("title", "content", widgets.ModalTypeErr, func(selectedOption string, remember bool) {
-//		}, options...)
 //	}
 //
 //	//notification := widgets.Notification{
@@ -145,9 +180,24 @@ package main
 //			gtx.Constraints.Min = gtx.Constraints.Max
 //			return layout.Flex{Axis: layout.Vertical}.Layout(gtx,
 //				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+//
+//					//return widgets.Button(th, clickable, widgets.ArrowDropDownIcon, 1, "click").Layout(gtx, theme)
+//					//func RadioButton(th *material.Theme, group *widget.Enum, key, label string) RadioButtonStyle {
+//					//e := &widget.Enum{
+//					//	Value: "test-value",
+//					//}
+//					//return widgets.RadioButton(th, e, "key", "value").Layout(gtx)
+//
+//					//return widgets.CheckBox(th, checkBox, "Remember me").Layout(gtx)
+//
 //					//return material.Button(th, clickable, "Close").Layout(gtx)
 //					//return data(gtx, th)
-//					return badgeInput.Layout(gtx, theme)
+//					//return badgeInput.Layout(gtx, theme)
+//					//return widgets.NewFileSelector("aaa.txt", explore, "*.go").Layout(gtx, theme)
+//
+//					//return flatButton.Layout(gtx, theme)
+//
+//					return widgets2.SaveButtonLayout(gtx, theme, clickable)
 //				}),
 //			)
 //		}),
---
+++ widgets/clickable.go
@@ -0,0 +1,29 @@
+package widgets
+
+import (
+	"image/color"
+
+	"gioui.org/io/semantic"
+	"gioui.org/layout"
+	"gioui.org/op/clip"
+	"gioui.org/op/paint"
+	"gioui.org/widget"
+)
+
+// Clickable lays out a rectangular clickable widget without further
+// decoration.
+func Clickable(gtx layout.Context, button *widget.Clickable, w layout.Widget) layout.Dimensions {
+	return button.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+		semantic.Button.Add(gtx.Ops)
+		return layout.Background{}.Layout(gtx,
+			func(gtx layout.Context) layout.Dimensions {
+				defer clip.Rect{Max: gtx.Constraints.Min}.Push(gtx.Ops).Pop()
+				if button.Hovered() {
+					paint.Fill(gtx.Ops, Hovered(color.NRGBA{}))
+				}
+				return layout.Dimensions{Size: gtx.Constraints.Min}
+			},
+			w,
+		)
+	})
+}
