5efae199b13f74b52ef9c85e81460f9808de231b
lijin <244395692@qq.com>
2024-08-04 00:20:05 +0800

    init

--- widgets/action.go
+++ widgets/action.go
@@ -67,12 +67,14 @@ func (c *Action) SetWidth(width int) {
 func (c *Action) Visible() bool {
 	return c.visible
 }
-func (c *Action) SetTitle(title string) {
+func (c *Action) SetTitle(title string) *Action {
 	c.title = title
+	return c
 }
 
-func (c *Action) SetHeight(height int) {
+func (c *Action) SetHeight(height int) *Action {
 	c.height = height
+	return c
 }
 
 func (c *Action) Message(message string) {
@@ -89,67 +91,66 @@ func (c *Action) Layout(gtx layout.Context) layout.Dimensions {
 	}
 	if c.visible {
 		// 绘制全屏半透明遮罩层
-		paint.Fill(gtx.Ops, color.NRGBA{R: 28, G: 28, B: 30, A: 245})
-		// paint.Fill(gtx.Ops, c.theme.Palette.Bg)
+		paint.Fill(gtx.Ops, c.theme.MaskLayerBgColor)
 	}
-
 	for c.actionClickable.Clicked(gtx) {
 		c.visible = false
 		if c.actionFunc != nil {
 			c.actionFunc()
 		}
 	}
-
 	width := gtx.Dp(unit.Dp(c.width))
 	height := gtx.Dp(unit.Dp(c.height))
-	return layout.Inset{
-		Top: unit.Dp(0),
-	}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-		return layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-			// Set the size of the Action
-			gtx.Constraints = layout.Exact(image.Point{X: width, Y: height})
-			rc := clip.RRect{
-				Rect: image.Rectangle{Max: image.Point{
-					X: gtx.Constraints.Min.X,
-					Y: gtx.Constraints.Min.Y,
-				}},
-				NW: 10, NE: 10, SE: 10, SW: 10,
-			}
-			paint.FillShape(gtx.Ops, c.theme.Palette.Fg, rc.Op(gtx.Ops))
-			// Center the text inside the Action
-			return layout.Flex{Axis: layout.Vertical}.Layout(gtx,
-				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-					return layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-						return layout.Inset{Left: 0, Right: 10, Bottom: 10, Top: 10}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-							return layout.Stack{Alignment: layout.N}.Layout(gtx,
-								layout.Stacked(func(gtx layout.Context) layout.Dimensions {
-									return Label(c.theme, c.title).Layout(gtx)
-								}),
-							)
+	return new(widget.Clickable).Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+		return layout.Inset{
+			Top: unit.Dp(0),
+		}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+			return layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+				// Set the size of the Action
+				gtx.Constraints = layout.Exact(image.Point{X: width, Y: height})
+				rc := clip.RRect{
+					Rect: image.Rectangle{Max: image.Point{
+						X: gtx.Constraints.Min.X,
+						Y: gtx.Constraints.Min.Y,
+					}},
+					NW: 10, NE: 10, SE: 10, SW: 10,
+				}
+				paint.FillShape(gtx.Ops, c.theme.Palette.Fg, rc.Op(gtx.Ops))
+				// Center the text inside the Action
+				return layout.Flex{Axis: layout.Vertical}.Layout(gtx,
+					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+						return layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+							return layout.Inset{Left: 0, Right: 10, Bottom: 10, Top: 10}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+								return layout.Stack{Alignment: layout.N}.Layout(gtx,
+									layout.Stacked(func(gtx layout.Context) layout.Dimensions {
+										return Label(c.theme, c.title).Layout(gtx)
+									}),
+								)
+							})
 						})
-					})
-				}),
-				DrawLineFlex(c.theme.Palette.Bg, unit.Dp(1), unit.Dp(c.width)),
-				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-					gtx.Constraints.Min.Y = gtx.Dp(unit.Dp(50))
-					return layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-						return layout.Inset{Left: 5, Right: 5, Bottom: 2, Top: 2}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-							label := material.Label(c.theme.Material(), c.theme.TextSize, c.message)
-							label.Color = theme2.LightBlue
-							label.Font.Weight = font.Medium
-							return label.Layout(gtx)
+					}),
+					DrawLineFlex(c.theme.Palette.Bg, unit.Dp(1), unit.Dp(c.width)),
+					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+						gtx.Constraints.Min.Y = gtx.Dp(unit.Dp(50))
+						return layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+							return layout.Inset{Left: 5, Right: 5, Bottom: 2, Top: 2}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+								label := material.Label(c.theme.Material(), c.theme.TextSize, c.message)
+								label.Color = theme2.LightBlue
+								label.Font.Weight = font.Medium
+								return label.Layout(gtx)
+							})
 						})
-					})
-				}),
-				DrawLineFlex(c.theme.SeparatorColor, unit.Dp(1), unit.Dp(c.width)),
-				layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
-					but := Button(c.theme, &c.actionClickable, nil, 1, "确 定", unit.Dp(50))
-					but.Background = c.theme.ConfirmButtonColor
-					but.width = unit.Dp(300)
-					return but.Layout(gtx, c.theme)
-				}),
-			)
+					}),
+					DrawLineFlex(c.theme.SeparatorColor, unit.Dp(1), unit.Dp(c.width)),
+					layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+						but := Button(c.theme, &c.actionClickable, nil, 1, "确 定", unit.Dp(50))
+						but.Background = c.theme.ConfirmButtonColor
+						but.width = unit.Dp(300)
+						return but.Layout(gtx, c.theme)
+					}),
+				)
 
+			})
 		})
 	})
 }
--- widgets/tips.go
+++ widgets/tips.go
@@ -13,7 +13,7 @@ import (
 	"gioui.org/op/clip"
 	"gioui.org/op/paint"
 	"gioui.org/unit"
-	"gioui.org/widget/material"
+	"gioui.org/widget"
 	"github.com/x-module/ui/theme"
 	"image"
 	"sync"
@@ -32,7 +32,7 @@ func NewTips(th *theme.Theme) *Tips {
 	}
 }
 
-func (t *Tips) Notify(message string) {
+func (t *Tips) Message(message string) {
 	t.message = message
 	t.visible = true
 }
@@ -50,39 +50,40 @@ func (t *Tips) Layout(gtx layout.Context) layout.Dimensions {
 	}
 	if t.visible {
 		// 绘制全屏半透明遮罩层
-		// fill := color.NRGBA{R: 0, G: 0, B: 0, A: 240} // 半透明黑色
-		// paint.FillShape(gtx.Ops, fill, clip.Rect{Max: gtx.Constraints.Max}.Op())
-		paint.Fill(gtx.Ops, t.theme.Palette.Bg)
+		paint.Fill(gtx.Ops, t.theme.MaskLayerBgColor)
 	}
 	// Define the size of the Tips
 	width := gtx.Dp(250)
 	height := gtx.Dp(50)
-	return layout.Inset{
-		Top: unit.Dp(0),
-	}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-		// Center the Tips in the parent container
-		return layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-			// Set the size of the Tips
-			gtx.Constraints = layout.Exact(image.Point{X: width, Y: height})
-			// 绘制全屏半透明遮罩层
-			// fill := color.NRGBA{R: 255, G: 255, B: 255, A: 255} // 半透明黑色
-			// paint.FillShape(gtx.Ops, fill, clip.Rect{Max: gtx.Constraints.Max}.Op())
-			rc := clip.RRect{
-				Rect: image.Rectangle{Max: image.Point{
-					X: gtx.Constraints.Min.X,
-					Y: gtx.Constraints.Min.Y,
-				}},
-				NW: 20, NE: 20, SE: 20, SW: 20,
-			}
-			paint.FillShape(gtx.Ops, t.theme.Palette.Bg, rc.Op(gtx.Ops))
-			// Center the text inside the Tips
-
+	return new(widget.Clickable).Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+		return layout.Inset{
+			Top: unit.Dp(0),
+		}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+			// Center the Tips in the parent container
 			return layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-				l := material.Label(t.theme.Material(), unit.Sp(16), "")
-				l.Font.Typeface = "MaterialIcons"
-				l.Color = t.theme.Material().Palette.Fg
-				l.Text = t.message
-				return l.Layout(gtx)
+				// Set the size of the Tips
+				gtx.Constraints = layout.Exact(image.Point{X: width, Y: height})
+				// 绘制全屏半透明遮罩层
+				// fill := color.NRGBA{R: 255, G: 255, B: 255, A: 255} // 半透明黑色
+				// paint.FillShape(gtx.Ops, fill, clip.Rect{Max: gtx.Constraints.Max}.Op())
+				rc := clip.RRect{
+					Rect: image.Rectangle{Max: image.Point{
+						X: gtx.Constraints.Min.X,
+						Y: gtx.Constraints.Min.Y,
+					}},
+					NW: 20, NE: 20, SE: 20, SW: 20,
+				}
+				paint.FillShape(gtx.Ops, t.theme.Palette.Fg, rc.Op(gtx.Ops))
+				// Center the text inside the Tips
+
+				return layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+					return BlueLabel(t.theme, t.message).Layout(gtx)
+					//l := material.Label(t.theme.Material(), unit.Sp(16), "")
+					//l.Font.Typeface = "MaterialIcons"
+					//l.Color = t.theme.TextColor
+					//l.Text = t.message
+					//return l.Layout(gtx)
+				})
 			})
 		})
 	})
